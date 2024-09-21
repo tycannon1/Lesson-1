b9433a27f367cbb5712ad407cd5cfc8a5472f764
Ty Cannon <cannont41@gmail.com>
2024-09-21 10:31:27 -0600

    adding lesson 1 assignment code

---
+++ server.js
@@ -0,0 +1,13 @@
+// express web server
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send("Hello12");
+});
+
+const port = 3000;
+ 
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+});
